8d71980c81f4a4322acafefc9da3a6312376b281
Uriel Aguirre <uriiaguirre11@outlook.com>
2022-08-03 12:40:36 -0300

    mvc

---
+++ routers/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+const mainControllers = require('../controllers/mainControllers');
+
+
+router.get('/',mainControllers.home);
+router.get('/about', mainControllers.about);
+
+module.exports = router;
